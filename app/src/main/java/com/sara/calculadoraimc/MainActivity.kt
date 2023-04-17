a3c0027d7caf184825f4632f52d68a98305aff7f
Sara <saraying03sb@gmail.com>
2023-04-17 13:22:35 +0200

    finished first acticity

--- app/src/main/java/com/sara/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/sara/calculadoraimc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sara.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -14,16 +16,20 @@ class MainActivity : AppCompatActivity() {
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
     private var currentWeight: Int = 60
+    private var currentAge: Int = 60
+    private var currentHeight: Int = 120
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
-
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
     private lateinit var btnSubtractWeight: FloatingActionButton
     private lateinit var btnPlusWeight: FloatingActionButton
     private lateinit var tvWeight: TextView
-
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var tvAge: TextView
+    private lateinit var  btnCalculate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +48,10 @@ class MainActivity : AppCompatActivity() {
         btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
         btnPlusWeight = findViewById(R.id.btnPlusWeight)
         tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        tvAge = findViewById(R.id.tvAge)
+        btnCalculate = findViewById(R.id.btnCalculate)
     }
 
     private fun initListeners() {
@@ -56,8 +66,8 @@ class MainActivity : AppCompatActivity() {
 
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
-            val result = df.format(value)
-            tvHeight.text = "$result cm"
+            val currentHeight = df.format(value).toInt()
+            tvHeight.text = "$currentHeight cm"
 
         }
         btnPlusWeight.setOnClickListener {
@@ -68,6 +78,30 @@ class MainActivity : AppCompatActivity() {
             currentWeight-=1
             setWeight()
         }
+
+        btnPlusAge.setOnClickListener {
+            currentAge+=1
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge-=1
+            setAge()
+        }
+
+        btnCalculate.setOnClickListener{
+            calculateIMC()
+        }
+    }
+
+    private fun calculateIMC(){
+        val df = DecimalFormat("#.##")
+        val imc = currentWeight/(currentHeight.toDouble()/100*currentHeight.toDouble()/100)
+        val result = df.format(imc).toDouble()
+        Log.i("Sara", "El imc es $imc")
+    }
+
+    private fun setAge(){
+        tvAge.text = currentAge.toString()
     }
 
     private fun setWeight(){
@@ -98,6 +132,7 @@ class MainActivity : AppCompatActivity() {
     private fun initUI() {
         setGenderColor()
         setWeight()
+        setAge()
     }
 
 }
