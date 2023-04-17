d050fc1cd92baf1bfcafc3cfb17e985eb80d4dc1
Sara <saraying03sb@gmail.com>
2023-04-17 12:00:52 +0200

    slider

--- app/src/main/java/com/sara/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/sara/calculadoraimc/MainActivity.kt
@@ -2,8 +2,11 @@ package com.sara.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
+
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     private fun initComponents(){
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun initListeners(){
@@ -34,6 +42,13 @@ class MainActivity : AppCompatActivity() {
         viewFemale.setOnClickListener {
             changeGender()
             setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
+
         }
     }
 
